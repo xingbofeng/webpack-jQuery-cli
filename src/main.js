3ac49ad4b790421ae1ae642c1a559cca1afeb31d
xingbofeng <503908971@qq.com>
2017-03-09 17:54:26 +0800

    feat: 增加404功能

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import style from './style.css';
 import $ from 'jquery';
 import routes from './routes.js';
 
+function mapRoutesToPage(routes) {
+  const path = window.location.pathname;
+  let i = 0;
+  while (i < routes.length) {
+    if (routes[i].path === path) {
+      return routes[i].components;
+    }
+    i++;
+  }
+  return '<h1>404 Not Found</h1>';
+}
+
 // 进行路由匹配
 $(document).ready(function() {
-  const path = window.location.pathname;
-  routes.map(v => {
-    if (v.path === path) {
-      $('body').html(v.components);
-    }
-  });
+  $('#container').html(mapRoutesToPage(routes));
 });
